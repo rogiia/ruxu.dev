5c016fa0f1bc56bfee34805e866dc244737cb173
Roger Oriol <rogi23696@gmail.com>
2025-02-24 19:33:30 +0100

    fix service-worker giving errors because of chrome extensions

--- service-worker.js
+++ service-worker.js
@@ -61,12 +61,16 @@ const fromCache = request =>
     );
 
 // cache the current page to make it available for offline
-const update = request =>
-  caches
+const update = request => {
+  if(!request.url.startsWith('http')) {
+    return;
+  }
+  return caches
     .open(CURRENT_CACHE)
     .then(cache =>
       fetch(request.url).then(response => cache.put(request, response))
     );
+}
 
 // general strategy when making a request (eg if online try to fetch it
 // from the network with a timeout, if something fails serve from cache)
